553274f5f726f31e6df2cd80455be2e9eb8e0d09
Christian Dionisio <christiandionisio9432@gmail.com>
2021-10-22 23:51:04 -0500

    ListView PaymentCards and refactoring

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,250 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:pedidos_app/utils/properties_util.dart';
+import 'package:pedidos_app/widgets/profile_page/account_settings_card.dart';
+import 'package:pedidos_app/widgets/profile_page/direction_card.dart';
+import 'package:pedidos_app/widgets/profile_page/logout_button.dart';
+import 'package:pedidos_app/widgets/profile_page/payment_card.dart';
+import 'package:pedidos_app/widgets/profile_page/profile_card.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          children: [
-            // Profile Card
-            Container(
-              decoration: _getCardDecoration(),
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              margin: const EdgeInsets.all(5),
-              height: 80,
-              child: ListTile(
-                leading: const ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(300)),
-                  child: Image(
-                    image: AssetImage('assets/home/perfil.png'),
-                    width: 44,
-                    height: 44,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                title: const Text(
-                  'Sadek Hossen',
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
-                ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 150,
-                        child: const Text(
-                          'sadekbranding@gmail.com',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          softWrap: false,
-                          style: TextStyle(fontSize: 12),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                trailing: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(300)),
-                  child: Container(
-                    color: Color(0xFFF9F9F9),
-                    child: IconButton(
-                      icon: Icon(Icons.edit),
-                      onPressed: () {
-                        // TODO: ir a editar usuario
-                        print('Editar usuario');
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          // Profile Card
+          ProrfileCard(),
+          SizedBox(height: 30),
 
-            // Accoun Settings Card
-            Container(
-              decoration: _getCardDecoration(),
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              margin: const EdgeInsets.all(5),
-              height: 80,
-              child: ListTile(
-                leading: Icon(
-                  Icons.account_circle_outlined,
-                  size: 35,
-                  color: Color(0xFF374151),
-                ),
-                title: const Text(
-                  'Account Settings',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Color(0xFF374151),
-                  ),
-                ),
-                trailing: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(300)),
-                  child: Container(
-                    child: IconButton(
-                      icon: Icon(Icons.edit),
-                      onPressed: () {
-                        // TODO: ir a editar usuario
-                        print('Editar usuario');
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
+          // Accoun Settings Card
+          AccountSettingsCard(),
 
-            // Direccion Card
-            Container(
-              decoration: _getCardDecoration(),
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              margin: const EdgeInsets.all(5),
-              height: 130,
-              child: ListTile(
-                leading: Icon(
-                  Icons.home_work_outlined,
-                  size: 80,
-                  color: Colors.lightBlue[200],
-                ),
-                title: const Text(
-                  'Sector 1 Grupo 13 Mz L Lt: 20',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                  softWrap: false,
-                ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 2),
-                      SizedBox(
-                        width: 160,
-                        child: const Text(
-                          'VILLA EL SALVADOR, LIMA, LIMA',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          softWrap: false,
-                          style: TextStyle(fontSize: 12),
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      SizedBox(
-                        width: 160,
-                        child: const Text(
-                          'Christian Dionisio Triveño',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          softWrap: false,
-                          style: TextStyle(fontSize: 12),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 160,
-                        child: const Text(
-                          'Teléfono: 999 999 999',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          softWrap: false,
-                          style: TextStyle(fontSize: 12),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                trailing: Container(
-                  child: IconButton(
-                    icon: Icon(Icons.edit_outlined),
-                    onPressed: () {
-                      // TODO: ir a editar usuario
-                      print('Editar usuario');
-                    },
-                  ),
-                ),
-              ),
-            ),
+          // Direccion Card
+          DirectionCard(),
+          SizedBox(height: 5),
 
-            // Tarjeta de pago Card
-            Container(
-              decoration: _getCardDecoration(),
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              margin: const EdgeInsets.all(5),
-              height: 130,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Logo tarjeta
-                      Image(
-                        image: AssetImage('assets/home/mastercard.png'),
-                        width: 44,
-                        fit: BoxFit.cover,
-                      ),
+          // Tarjeta de pago Card
+          _LabelListCustomCard(),
+          _ListViewPaymentCard(),
+          SizedBox(height: 20),
 
-                      // Eliminar tarjeta
-                      Material(
-                        color: Colors.white,
-                        child: InkWell(
-                          customBorder: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          onTap: () {
-                            // TODO: ELIMINAR
-                            print("Eliminarr");
-                          },
-                          child: Container(
-                            width: 40,
-                            height: 40,
-                            child: Icon(Icons.delete_outline_sharp),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 7),
-                  Text("XXXX XXXX XXXX 4567"),
-                  SizedBox(height: 7),
-                  Text("Vigencia   09/2025"),
-                  SizedBox(height: 5),
-                  Text("CHRISTIAN DIONISIO TRIVEÑO"),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-
-            // Logout Button
-            Container(
-              width: 111,
-              child: ElevatedButton(
-                child: Text("Logout"),
-                onPressed: () {
-                  // TODO: LOGOUT
-                },
-                style: PropertiesUtil.styleDangerButton,
-              ),
-            ),
-          ],
-        ),
+          // Logout Button
+          LogoutButton(),
+        ],
       ),
     );
   }
+}
 
-  BoxDecoration _getCardDecoration() {
-    return const BoxDecoration(
-      borderRadius: BorderRadius.all(Radius.circular(10)),
-      color: Colors.white,
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey,
-          offset: Offset(0.0, 1.0), //(x,y)
-          blurRadius: 1.0,
-        ),
-      ],
+class _ListViewPaymentCard extends StatelessWidget {
+  const _ListViewPaymentCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 140,
+      child: ListView.builder(
+          itemCount: 3,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return PaymentCard();
+          }),
+    );
+  }
+}
+
+class _LabelListCustomCard extends StatelessWidget {
+  const _LabelListCustomCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Tarjetas Guardadas"),
+          InkWell(
+            child: Container(
+              width: 30,
+              height: 30,
+              child: Icon(Icons.add),
+            ),
+            onTap: () {},
+            customBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/utils/properties_util.dart
+++ lib/utils/properties_util.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class PropertiesUtil {
   static const Color primaryColor = Color(0xFF32B768);
+  static const Color dangerColor = Color(0xFFEB4646);
   static const Color primaryColorOutlined = Color(0xFFD1FAE5);
   static const Color colorGoogleButtonStyle = Color(0xFFF4F4F4);
 
@@ -22,9 +23,20 @@ class PropertiesUtil {
   );
 
   static final ButtonStyle styleDangerButton = ButtonStyle(
-    backgroundColor:
-        MaterialStateColor.resolveWith((states) => Color(0xFFEB4646)),
+    backgroundColor: MaterialStateColor.resolveWith((states) => dangerColor),
     shape: MaterialStateProperty.all(
         RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0))),
+  );
+
+  static final BoxDecoration getCardDecoration = BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    color: Colors.white,
+    boxShadow: [
+      BoxShadow(
+        color: Colors.grey,
+        offset: Offset(0.0, 1.0), //(x,y)
+        blurRadius: 1.0,
+      ),
+    ],
   );
 }
